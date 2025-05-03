cd39be5623ba55f375d6c27f3a9dca2173a8a06f
jpbuenos <119616181+jpbuenos@users.noreply.github.com>
2025-05-03 13:35:39 -0300

    Add files via upload

---
+++ bot_furia.py
@@ -0,0 +1,98 @@
+# Importando a bibliblioteca dotenv para proteger o token do bot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+import telebot
+
+# Bot com o token do BotFather
+bot = telebot.TeleBot('7268221173:AAEfGnjW2u6dWcGAoTb9ZcZgHQ4CViI69nk')
+
+# Comandos que o bot escuta
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(msg):
+    bot.send_message(msg.chat.id, "Olá! Sou o bot da FURIA, pronto para te ajudar! \n \n"
+                                  "Comandos disponíveis:\n"
+                                  "/elenco - Ver elenco atual da line de CS\n"
+                                  "/jogos - Ver próximos jogos\n"
+                                  "/jogosrecentes - Ver resultados de jogos recentes\n"
+                                  "/campeonatos - Ver campeonatos que a line está participando ou participará\n"
+                                  "/winrate - Ver winrate nos mapas da atual line de CS\n"
+                                  "/ranking - Ver ranking atual da FURIA\n")  
+
+# Comando para mostrar o atual elenco da line de CS
+@bot.message_handler(commands=['elenco'])
+def elenco(msg):
+    bot.send_message(msg.chat.id, "Elenco atual da line de CS:\n\n"
+                                  "Titulares:\n"
+                                  "1. MOLODOY\n"
+                                  "2. YEKINDAR\n"
+                                  "3. FalleN\n"
+                                  "4. KSCERATO\n"
+                                  "5. yurrih\n\n"
+                                  "Reservas:\n"
+                                  "1. skullz\n"
+                                  "2. chelo\n\n"
+                                  "Coachs:\n"
+                                  "1. hepa\n"
+                                  "2. sidde\n")
+# Comando para mostrar os próximos jogos
+@bot.message_handler(commands=['jogos'])
+def jogos(msg):
+    bot.send_message(msg.chat.id, "Próximos jogos:\n"
+                                  "Nenhum jogo encontrado para os próximos dias.\n")
+
+# Comando para mostrar os resultados de jogos recentes
+@bot.message_handler(commands=['jogosrecentes'])
+def jogosrecentes(msg):
+    bot.send_message(msg.chat.id, "Resultados de jogos recentes:\n"
+                                  "FURIA x The MongolZ - 09/04/2025 às 09:50 - 0 x 2\n"
+                                  "FURIA x Virtus.pro - 08/04/2025 às 06:05 - 0 x 2\n"
+                                  "FURIA x Complexity - 07/04/2025 às 11:05 - 1 x 2\n"
+                                  "FURIA x Apogee - 06/04/2025 às 09:50 - 2 x 0\n")
+    
+# Comando para mostrar os campeonatos que a line está participando ou participará
+@bot.message_handler(commands=['campeonatos'])
+def campeonatos(msg):
+    bot.send_message(msg.chat.id, "Campeonatos que a line está participando ou participará:\n"
+                                  "PGL Astana 2025 - 10/05/2025\n"
+                                  "IEM Dallas 2025 - 19/05/2025\n"
+                                  "BLAST.tv Austir Major 2025 Stage 2 - 07/06/2025\n")
+
+# Comando para mostrar o winrate dos mapas
+@bot.message_handler(commands=['winrate'])
+def winrate(msg):
+    bot.send_message(msg.chat.id, "Winrate dos mapas abaixo:\n"
+                                  "Cache - 73.2%\n"
+                                  "Cobblestone - 70.6%\n"
+                                  "Train - 62.5%\n"
+                                  "Vertigo - 61.2%\n"
+                                  "Mirage - 60.8%\n"
+                                  "Overpass - 60.3%\n"
+                                  "Inferno - 59.1%\n"
+                                  "Dust2 - 56.8%\n"
+                                  "Nuke - 56.1%\n"
+                                  "Ancient - 51.5%\n"
+                                  "Anubis - 44.4%\n")
+
+# Comando para mostrar o ranking atual da FURIA    
+@bot.message_handler(commands=['ranking'])
+def ranking(msg):
+    bot.send_message(msg.chat.id, "Ranking mundial até o momento:\n"
+                                  "World Ranking:\n"
+                                  "Vitality #1\n"
+                                  "Spirit #2\n"
+                                  "MOUZ #3\n"
+                                  "Natus Vincere #4\n"
+                                  "...\n"
+                                  "Furia #17\n\n"
+                                  "Observação: O ranking é atualizado semanalmente e pode mudar a qualquer momento, para mais informações, acesse: https://www.hltv.org/\n")
+
+
+
+# Bot escutando
+print("Bot está online...")
+bot.infinity_polling()
